d9c8b8c66a826fec753a70d42fa0acf76a86e6f6
Mahfuz60 <mahfuzuralam1996@gmail.com>
2026-05-26 01:20:40 +0600

    build-code

--- android/app/src/main/java/com/focuszen/app/MainActivity.kt
+++ android/app/src/main/java/com/focuszen/app/MainActivity.kt
@@ -40,11 +40,6 @@ class MainActivity : ReactActivity() {
           ){})
   }
 
-  override fun onNewIntent(intent: android.content.Intent) {
-    super.onNewIntent(intent)
-    setIntent(intent)
-  }
-
   /**
     * Align the back button behavior with Android S
     * where moving root activities to background instead of finishing activities.
